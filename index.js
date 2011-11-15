060c64991afbeb1e8509ac7a459f2b93c4c9abc1
Agustin Hernandez <ahernandez@avantic.net>
2011-11-15 12:02:42 +0000

    Commit inicial

---
+++ index.js
@@ -0,0 +1,16 @@
+﻿
+function onLoad() {
+	alert("¡Hola Mundo!");
+	
+	var txt = "<p>Navegador: " + navigator.appVersion + "</p>";
+		txt+= "<p>S.O.: " + navigator.platform + "</p>";
+
+	document.getElementById("info").innerHTML = txt;
+}
+
+function toggleText() {
+	if (document.getElementById('text').style.display == "none")
+		document.getElementById('text').style.display = "";
+	else if (document.getElementById('text').style.display == "")
+		document.getElementById('text').style.display = "none";
+}
